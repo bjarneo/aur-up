45a860255c33d2a8bdb6b0599bf19f3e784b3c44
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2025-08-14 22:04:06 +0200

    Update

--- main.go
+++ main.go
@@ -21,15 +21,9 @@ type UptimeRobotResponse struct {
 }
 
 func clearScreen() {
-	if runtime.GOOS == "windows" {
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
 		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-	}
 }
 
 func main() {
